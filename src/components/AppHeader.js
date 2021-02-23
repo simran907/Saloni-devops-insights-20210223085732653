858e2a079f612772ab415470ee3c608de351eded
Simran Patel <simranpatel18@gnu.ac.in>
2021-02-23 09:02:46 +0000

    First Commit by Saloni Doshi

--- src/components/AppHeader.js
+++ src/components/AppHeader.js
@@ -2,8 +2,9 @@ import React from 'react';
 
 function AppHeader() {
 
-    return <div>
-        <p className="p-3 text-light h3 bg-primary">Current weather by zip code</p>
+    return 
+    <div>
+        <p className="p-3 text-light h3 bg-primary">Saloni Doshi </p>
     </div>
 }
   
